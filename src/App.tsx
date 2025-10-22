df2b0b2d67952ca1cdd485d3a3584e98e3144b2f
LuizDev544 <luizfernando.carrupt@gmail.com>
2025-10-22 16:12:41 -0300

    opa

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react'
 import Header from './components/Header'
 import Produtos from './containers/Produtos'
-
 import { GlobalStyle } from './styles'
+import { Provider } from 'react-redux'
+import { store } from './components/store'
 
 export type Game = {
   id: number
@@ -23,15 +24,17 @@ function App() {
       .then((res) => res.json())
       .then((res) => setGames(res))
   }, [])
-
+  function adicionarAoCarrinho() {
+    console.log('teste')
+  }
   return (
-    <>
+    <Provider store={store}>
       <GlobalStyle />
       <div className="container">
-        <Header itensNoCarrinho={carrinho} />
+        <Header />
         <Produtos jogos={games} adicionarAoCarrinho={adicionarAoCarrinho} />
       </div>
-    </>
+    </Provider>
   )
 }
 
